1ff7346f646845fa9dc7790f7717e026bb2207a4
madiks <madiks@163.com>
2015-03-12 10:31:35 +0800

    update Exported/Unexported identifiers in this package

--- goMapReduce/goMapReduce.go
+++ goMapReduce/goMapReduce.go
@@ -9,18 +9,18 @@ type MRChanData struct {
 	Value interface{}
 }
 
-type UserCustomMapReduce interface {
+type userCustomMapReduce interface {
 	CustomMap(mapData interface{}, mapOutChannel chan MRChanData)
 
 	CustomReduce(key string, values []interface{}, reduceOutChannel chan MRChanData)
 }
 
-type MapReduce struct {
+type mapReduce struct {
 	mapInChannel     chan MRChanData
 	mapOutChannel    chan MRChanData
 	reduceInChannel  chan MRChanData
 	reduceOutChannel chan MRChanData
-	customMapReduce  *UserCustomMapReduce
+	customMapReduce  *userCustomMapReduce
 
 	aggregate map[string][]interface{}
 	//reduce start Critical Value
@@ -29,8 +29,8 @@ type MapReduce struct {
 	maxReducerNum       int
 }
 
-func NewMapReduce(uc UserCustomMapReduce, reduceCriticalValue int, maxMapper int, maxReducer int) *MapReduce {
-	mapReduce := &MapReduce{
+func NewMapReduce(uc userCustomMapReduce, reduceCriticalValue int, maxMapper int, maxReducer int) *mapReduce {
+	varMapReduce := &mapReduce{
 		mapInChannel:        make(chan MRChanData),
 		mapOutChannel:       make(chan MRChanData),
 		reduceInChannel:     make(chan MRChanData),
@@ -41,73 +41,73 @@ func NewMapReduce(uc UserCustomMapReduce, reduceCriticalValue int, maxMapper int
 		maxMapperNum:        maxMapper,
 		maxReducerNum:       maxReducer,
 	}
-	return mapReduce
+	return varMapReduce
 }
 
-func (mapReduce *MapReduce) GetMapInChannel() (mapInChannel chan MRChanData) {
-	return mapReduce.mapInChannel
+func (varMapReduce *mapReduce) GetMapInChannel() (mapInChannel chan MRChanData) {
+	return varMapReduce.mapInChannel
 }
 
-func (mapReduce *MapReduce) GetreduceOutChannel() (mapInChannel chan MRChanData) {
-	return mapReduce.reduceOutChannel
+func (varMapReduce *mapReduce) GetreduceOutChannel() (mapInChannel chan MRChanData) {
+	return varMapReduce.reduceOutChannel
 }
 
-func (mapReduce *MapReduce) ListenMapIn() {
+func (varMapReduce *mapReduce) listenMapIn() {
 	var mapWaitGroup sync.WaitGroup
 	// start X goroutines do map
-	for i := 0; i < mapReduce.maxMapperNum; i++ {
+	for i := 0; i < varMapReduce.maxMapperNum; i++ {
 		mapWaitGroup.Add(1)
 		go func() {
-			for mapInData := range mapReduce.mapInChannel {
-				(*mapReduce.customMapReduce).CustomMap(mapInData.Value, mapReduce.mapOutChannel)
+			for mapInData := range varMapReduce.mapInChannel {
+				(*varMapReduce.customMapReduce).CustomMap(mapInData.Value, varMapReduce.mapOutChannel)
 			}
 			mapWaitGroup.Done()
 		}()
 	}
 	//when all map task done close mapOut channel
 	mapWaitGroup.Wait()
-	close(mapReduce.mapOutChannel)
+	close(varMapReduce.mapOutChannel)
 }
 
-func (mapReduce *MapReduce) aggregateMapOut() {
-	for mapOutData := range mapReduce.mapOutChannel {
-		if len(mapReduce.aggregate[mapOutData.Key]) == mapReduce.reduceCriticalValue {
-			mapReduce.reduceInChannel <- MRChanData{mapOutData.Key, mapReduce.aggregate[mapOutData.Key]}
-			mapReduce.aggregate[mapOutData.Key] = mapReduce.aggregate[mapOutData.Key][:0]
+func (varMapReduce *mapReduce) aggregateMapOut() {
+	for mapOutData := range varMapReduce.mapOutChannel {
+		if len(varMapReduce.aggregate[mapOutData.Key]) == varMapReduce.reduceCriticalValue {
+			varMapReduce.reduceInChannel <- MRChanData{mapOutData.Key, varMapReduce.aggregate[mapOutData.Key]}
+			varMapReduce.aggregate[mapOutData.Key] = varMapReduce.aggregate[mapOutData.Key][:0]
 		}
-		mapReduce.aggregate[mapOutData.Key] = append(mapReduce.aggregate[mapOutData.Key], mapOutData.Value)
+		varMapReduce.aggregate[mapOutData.Key] = append(varMapReduce.aggregate[mapOutData.Key], mapOutData.Value)
 	}
 	//when mapOutChannel close,send the rest of data to do reduce
-	for k := range mapReduce.aggregate {
-		mapReduce.reduceInChannel <- MRChanData{k, mapReduce.aggregate[k]}
+	for k := range varMapReduce.aggregate {
+		varMapReduce.reduceInChannel <- MRChanData{k, varMapReduce.aggregate[k]}
 	}
 
-	close(mapReduce.reduceInChannel)
+	close(varMapReduce.reduceInChannel)
 }
 
-func (mapReduce *MapReduce) ListenReduceIn() {
+func (varMapReduce *mapReduce) listenReduceIn() {
 	var reduceWaitGroup sync.WaitGroup
 	// start X goroutines to do reduce
-	for i := 0; i < mapReduce.maxReducerNum; i++ {
+	for i := 0; i < varMapReduce.maxReducerNum; i++ {
 		reduceWaitGroup.Add(1)
 		go func() {
-			for reduceInData := range mapReduce.reduceInChannel {
-				(*mapReduce.customMapReduce).CustomReduce(reduceInData.Key, reduceInData.Value.([]interface{}), mapReduce.reduceOutChannel)
+			for reduceInData := range varMapReduce.reduceInChannel {
+				(*varMapReduce.customMapReduce).CustomReduce(reduceInData.Key, reduceInData.Value.([]interface{}), varMapReduce.reduceOutChannel)
 			}
 			reduceWaitGroup.Done()
 		}()
 	}
 	//when all reducer goroutine done close reduceOut channel
 	reduceWaitGroup.Wait()
-	close(mapReduce.reduceOutChannel)
+	close(varMapReduce.reduceOutChannel)
 }
 
-func (mapReduce *MapReduce) Run() {
+func (varMapReduce *mapReduce) Run() {
 
-	go mapReduce.ListenMapIn()
+	go varMapReduce.listenMapIn()
 
-	go mapReduce.aggregateMapOut()
+	go varMapReduce.aggregateMapOut()
 
-	go mapReduce.ListenReduceIn()
+	go varMapReduce.listenReduceIn()
 
 }
